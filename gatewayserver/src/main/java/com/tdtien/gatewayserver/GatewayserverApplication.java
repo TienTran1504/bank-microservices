4424a389055989a5edc3bf204ad19be353db9c8a
tdtien <tdtien.work@gmail.com>
2025-12-24 22:40:03 +0700

    feat: implementing ratelimiter pattern in gateway

--- gatewayserver/src/main/java/com/tdtien/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/tdtien/gatewayserver/GatewayserverApplication.java
@@ -6,11 +6,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -44,7 +47,9 @@ public class GatewayserverApplication {
 				.route( p -> p
 						.path("/bank/cards/**")
 						.filters(f -> f.rewritePath("/bank/cards/(?<segment>.*)", "/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+								.setKeyResolver(userKeyResolver())))
 						.uri("lb://CARDS"))
 				.build();
 	}
@@ -55,6 +60,17 @@ public class GatewayserverApplication {
 				.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build())
 				.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
 				.build());
+	}
+
+	@Bean
+	public RedisRateLimiter redisRateLimiter() {
+		return new RedisRateLimiter(1, 1,1);
+	}
+
+	@Bean
+	public KeyResolver userKeyResolver() {
+		return exchange -> Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("user"))
+				.defaultIfEmpty("anonymous");
 	}
 
 }
